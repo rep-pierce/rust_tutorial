f078c640875d4eb4e401e211c419e28a6a1edc97
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-21 16:44:30 -0600

    while loop

--- src/main.rs
+++ src/main.rs
@@ -115,18 +115,27 @@ use std::cmp::Ordering;
 //     println!("Length : {}", arr_1.len());
 // }
 
+// fn main() {
+//     let arr_2 = [1,2,3,4,5,6,7,8,9];
+//     let mut loop_idx = 0;
+//     loop {
+//         if arr_2[loop_idx] % 2 == 0 {
+//             loop_idx +=1;
+//             continue
+//         }
+//         if arr_2[loop_idx] == 9{
+//             break;
+//         }
+//         println!("Val : {}", arr_2[loop_idx]);
+//         loop_idx +=1;
+//     }
+// }
+
 fn main() {
     let arr_2 = [1,2,3,4,5,6,7,8,9];
     let mut loop_idx = 0;
-    loop {
-        if arr_2[loop_idx] % 2 == 0 {
-            loop_idx +=1;
-            continue
-        }
-        if arr_2[loop_idx] == 9{
-            break;
-        }
-        println!("Val : {}", arr_2[loop_idx]);
+    while loop_idx < arr_2.len(){
+        println!("Arr : {}", arr_2[loop_idx]);
         loop_idx +=1;
     }
 }
