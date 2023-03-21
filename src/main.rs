b4f174d160ded419a3b9a5f9aa347ff4132852e6
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-21 16:41:43 -0600

    loops

--- src/main.rs
+++ src/main.rs
@@ -109,8 +109,24 @@ use std::cmp::Ordering;
 //     };
 // }
 
+// fn main() {
+//     let arr_1 = [1,2,3,4];
+//     println!("1st : {}", arr_1[0]);
+//     println!("Length : {}", arr_1.len());
+// }
+
 fn main() {
-    let arr_1 = [1,2,3,4];
-    println!("1st : {}", arr_1[0]);
-    println!("Length : {}", arr_1.len());
+    let arr_2 = [1,2,3,4,5,6,7,8,9];
+    let mut loop_idx = 0;
+    loop {
+        if arr_2[loop_idx] % 2 == 0 {
+            loop_idx +=1;
+            continue
+        }
+        if arr_2[loop_idx] == 9{
+            break;
+        }
+        println!("Val : {}", arr_2[loop_idx]);
+        loop_idx +=1;
+    }
 }
