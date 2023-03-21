d16ed9d7c969b450d0e6d0c4049073817347abdb
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-21 16:16:21 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,20 @@ use std::cmp::Ordering;
 //     let my_grade = 'A';
 // }
 
+// fn main() {
+//     let num_1: f32 = 1.111111111111111;
+//     println!("f32 : {}", num_1 + 0.111111111111111); //answer: f32 : 1.2222223
+//     let num_2: f64 = 1.111111111111111;
+//     println!("f364 : {}", num_2 + 0.111111111111111); //answer: f64 : 1.2222222222222219
+// }
+
 fn main() {
-    let num_1: f32 = 1.111111111111111;
-    println!("f32 : {}", num_1 + 0.111111111111111); //answer: f32 : 1.2222223
-    let num_2: f64 = 1.111111111111111;
-    println!("f364 : {}", num_2 + 0.111111111111111); //answer: f64 : 1.2222222222222219
+    let mut num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+    num_3 += 1; //can be done with any of the above math operators so like num_3 *= 3
 }
