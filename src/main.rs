9b5e4dc3ff071e8393156e30240ff68cc297e17a
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-21 16:01:06 -0600

    initial

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+// fn main() {
+//     println!("What is your name?");
+//     let mut name = String::new();
+//     let greeting = "Nice to meet you";
+//     io::stdin().read_line(&mut name)
+//         .expect("Didn't Recieve Input");
+
+//     println!("Hello {}! {}", name.trim_end(), greeting);
+// }
+
+fn main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age = "47";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
+}
