646aee460fb0745931009802d0750958c6a14404
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-21 16:09:06 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,19 @@ use std::cmp::Ordering;
 //     println!("I'm {} and I want ${}", age, ONE_MIL);
 // }
 
+// fn main() {
+//     // Unsigned integer : u8, u16, u32, u64, u128, usize
+//     // Signed integer : i8, i16, i32, i64, i128, isize
+//     println!("Max u32 : {}", u32::MAX);
+//     println!("Max u64 : {}", u64::MAX);
+//     println!("Max u128 : {}", u128::MAX);
+//     println!("Max usize : {}", usize::MAX);
+//     println!("Max u32 : {}", u32::MAX);
+//     println!("Max f32 : {}", f32::MAX);
+//     println!("Max f64 : {}", f64::MAX);
+// }
+
 fn main() {
-    // Unsigned integer : u8, u16, u32, u64, u128, usize
-    // Signed integer : i8, i16, i32, i64, i128, isize
-    println!("Max u32 : {}", u32::MAX);
-    println!("Max u64 : {}", u64::MAX);
-    println!("Max u128 : {}", u128::MAX);
-    println!("Max usize : {}", usize::MAX);
-    println!("Max u32 : {}", u32::MAX);
-    println!("Max f32 : {}", f32::MAX);
-    println!("Max f64 : {}", f64::MAX);
+    let is_true = true;
+    let my_grade = 'A';
 }
